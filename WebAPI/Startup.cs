98c92c1f3a8ae7648f7a2c6d169f6367095ac0a5
yasin Özbey <yasin_2313@hotmail.com>
2021-03-11 00:29:18 +0300

    11 mart ders sonu

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -44,8 +44,8 @@ namespace WebAPI
             services.AddControllers();
             //services.AddSingleton<IProductService,ProductManager>();
             //services.AddSingleton<IProductDal, EfProductDal>();
-            
-            
+
+            services.AddCors();
 
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
@@ -77,6 +77,8 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
             
